7fb8c230e0c2a9d6a4f40623c18da3b007594a3d
DavidPuerto <tutos6782@gmail.com>
2025-04-22 11:39:55 +0200

    Hibernate añadido.

---
+++ AutoTechTalleres/src/main/java/com/davidpuertocuenta/autotechtalleres/dao/HibernateUtil.java
@@ -0,0 +1,34 @@
+package com.davidpuertocuenta.autotechtalleres.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * Clase destinada a configuración para el gestor de datos
+ * sql de hibernate.
+ *
+ * @author David Puerto Cuenca
+ * @version 1.0
+ */
+
+public class HibernateUtil {
+    
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try (StandardServiceRegistry registry = new
+                    StandardServiceRegistryBuilder().build();) {
+                sessionFactory = new MetadataSources(registry)
+                        //.addAnnotatedClass(Citas.class)
+                        .buildMetadata().buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+}
